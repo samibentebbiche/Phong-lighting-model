332dea4fd39df703ac4d16a34ed3cf202a81063e
sami <sami@pop-os.localdomain>
2024-10-19 23:53:19 +0200

    add CmakeLists.txt

---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/src/Ibo.cpp.o"
+  "CMakeFiles/main.dir/src/Ibo.cpp.o.d"
+  "CMakeFiles/main.dir/src/Renderer.cpp.o"
+  "CMakeFiles/main.dir/src/Renderer.cpp.o.d"
+  "CMakeFiles/main.dir/src/Shader.cpp.o"
+  "CMakeFiles/main.dir/src/Shader.cpp.o.d"
+  "CMakeFiles/main.dir/src/Texture.cpp.o"
+  "CMakeFiles/main.dir/src/Texture.cpp.o.d"
+  "CMakeFiles/main.dir/src/Vao.cpp.o"
+  "CMakeFiles/main.dir/src/Vao.cpp.o.d"
+  "CMakeFiles/main.dir/src/Vbo.cpp.o"
+  "CMakeFiles/main.dir/src/Vbo.cpp.o.d"
+  "CMakeFiles/main.dir/src/VertexBufferLayout.cpp.o"
+  "CMakeFiles/main.dir/src/VertexBufferLayout.cpp.o.d"
+  "CMakeFiles/main.dir/src/test.cpp.o"
+  "CMakeFiles/main.dir/src/test.cpp.o.d"
+  "CMakeFiles/main.dir/src/vendor/stb_image/stb_image.cpp.o"
+  "CMakeFiles/main.dir/src/vendor/stb_image/stb_image.cpp.o.d"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
